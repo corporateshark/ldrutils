93b2adbf7c7fb054f6a8f0122e7f03b100882293
Sergey Kosarevsky <sk@linderdaum.com>
2023-04-02 14:44:08 -0700

    Even more math helper functions

--- lmath/Math.h
+++ lmath/Math.h
@@ -14,6 +14,7 @@
 #pragma once
 
 #include <stdint.h>
+#include <cmath>
 
 #include "lutils/Macros.h"
 
@@ -27,8 +28,6 @@
 #define LMATH_DTOR  0.01745329251994329577f
 // 180/Pi
 #define LMATH_RTOD 57.29577951308232087679f
-// ln(0.5)^-1
-#define LMATH_LOGHALFINV -1.44269504088896340736f
 
 namespace ldr
 {
@@ -66,6 +65,11 @@ LFORCEINLINE void swapf(float& v1, float& v2)
 	v2 = tmp;
 }
 
+LFORCEINLINE float signf(float x)
+{
+	return (x > 0) ? 1.0f : -1.0f;
+}
+
 // note: returns `true` for 0
 LFORCEINLINE [[nodiscard]] bool isPowerOf2(uint32_t n)
 {
@@ -99,6 +103,81 @@ LFORCEINLINE [[nodiscard]] uint64_t getNextPowerOf2(uint64_t n)
 	n |= (n >> 16);
 	n |= (n >> 32);
 	return n + 1;
+}
+
+// floating-point division remainder (wrap around negative values)
+LFORCEINLINE [[nodiscard]] float modf(float a, float b)
+{
+	a -= b * floor(a / b);
+	return (a < 0) ? a + b : a;
+}
+
+// integer division remainder (wrap around negative values)
+LFORCEINLINE [[nodiscard]] int modi(int a, int b)
+{
+	const int r = a % b;
+	return (r < 0) ? r + b : r;
+}
+
+template <typename T> LFORCEINLINE [[nodiscard]] T clamp(T x, T minVal, T maxVal)
+{
+	if (x > maxVal)
+		return maxVal;
+	if (x < minVal)
+		return minVal;
+	return x;
+}
+
+LFORCEINLINE [[nodiscard]] float clampfPeriodic(float x, float minVal, float maxVal)
+{
+	if (x > maxVal)
+		return minVal + ldr::modf(x - minVal, maxVal - minVal);
+	if (x < minVal)
+		return maxVal - ldr::modf(minVal - x, maxVal - minVal);
+	return x;
+}
+
+// GLSL step()
+LFORCEINLINE [[nodiscard]] float step(float edge, float x)
+{
+	return x < edge ? 0.0f : 1.0f;
+}
+
+// GLSL smoothstep()
+LFORCEINLINE [[nodiscard]] float smoothStep(float edge0, float edge1, float x)
+{
+	if (x <= edge0)
+		return 0.0f;
+	if (x >= edge1)
+		return 1.0f;
+	const float t = (x - edge0) / (edge1 - edge0);
+	return t * t * (3.0f - 2.0f * t);
+}
+
+LFORCEINLINE [[nodiscard]] float lerp(float a, float b, float x)
+{
+	return std::fmaf(x, b - a, a);
+}
+
+LFORCEINLINE [[nodiscard]] double lerp(double a, double b, float x)
+{
+	return std::fma(x, b - a, a);
+}
+
+template <class T> LFORCEINLINE [[nodiscard]] T lerp(const T& a, const T& b, float x)
+{
+	return x * (b - a) + a;
+}
+
+// note: degrees
+template <class T> LFORCEINLINE T clipAngleTo0_360(T angle)
+{
+	return std::fmod(angle, T(360));
+}
+
+LFORCEINLINE float vecToAngle(float x, float y)
+{
+	return 180.0f + ldr::radToDeg(atan2(y, x));
 }
 
 } // namespace ldr
