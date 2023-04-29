12fcfbe3a8c3388fa5815fb4695aeed4c0e76f63
Sergey Kosarevsky <sk@linderdaum.com>
2023-04-29 02:57:44 -0700

    Added helpful vec4 colors

---
+++ lmath/Colors.h
@@ -0,0 +1,30 @@
+﻿/**
+ * \file Colors.h
+ * \brief
+ *
+ * Common colors definitions
+ *
+ * \author Sergey Kosarevsky, 2023
+ * \author support@linderdaum.com   http://www.linderdaum.com   http://blog.linderdaum.com
+ * https://github.com/corporateshark/lutils
+ */
+
+#pragma once
+
+// clang-format off
+#define LC_Transparent vec4(0.0f, 0.0f, 0.0f, 0.0f)
+#define LC_Black       vec4(0.0f, 0.0f, 0.0f, 1.0f)
+#define LC_White       vec4(1.0f, 1.0f, 1.0f, 1.0f)
+#define LC_Aqua        vec4(0.0f, 1.0f, 1.0f, 1.0f)
+#define LC_Blue        vec4(0.0f, 0.0f, 1.0f, 1.0f)
+#define LC_Cyan        vec4(0.0f, 1.0f, 1.0f, 1.0f)
+#define LC_Fuchsia     vec4(1.0f, 0.0f, 1.0f, 1.0f)
+#define LC_Gray        vec4(0.7f, 0.7f, 0.7f, 1.0f)
+#define LC_Green       vec4(0.0f, 1.0f, 0.0f, 1.0f)
+#define LC_Lime        vec4(0.0f, 1.0f, 0.0f, 1.0f)
+#define LC_Olive       vec4(0.0f, 1.0f, 1.0f, 1.0f)
+#define LC_Purple      vec4(1.0f, 0.0f, 1.0f, 1.0f)
+#define LC_Red         vec4(1.0f, 0.0f, 0.0f, 1.0f)
+#define LC_Teal        vec4(0.0f, 1.0f, 1.0f, 1.0f)
+#define LC_Yellow      vec4(1.0f, 1.0f, 0.0f, 1.0f)
+// clang-format on
