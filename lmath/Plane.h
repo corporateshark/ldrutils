56a5510ca15c8a93ffd28736e108fd425df6f6d2
Sergey Kosarevsky <sk@linderdaum.com>
2023-04-29 01:17:14 -0700

    Added plane3 helper functions

--- lmath/Plane.h
+++ lmath/Plane.h
@@ -19,33 +19,60 @@ namespace ldr
 class plane3
 {
  public:
-	vec4 plane;
+	union {
+		vec4 p;
+		struct {
+			vec3 n;
+			float d;
+		};
+	};
 
  public:
 	plane3()
-	: plane(0.0f, 0.0f, 1.0f, 0.0f){};
-
+	: p(0.0f, 0.0f, 1.0f, 0.0f){};
 	/// from a plane equation
 	explicit plane3(const vec4& v)
-	: plane(v){};
+	: p(v){};
 	/// from a normal and parameter
 	explicit plane3(const vec3& normal, float d)
-	: plane(normal, d){};
+	: p(normal, d){};
 	/// from a plane equation
 	plane3(float nx, float ny, float nz, float d)
-	: plane(vec3(nx, ny, nz), d){};
+	: p(vec3(nx, ny, nz), d){};
 	/// from a point and normal
 	plane3(const vec3& pt, const vec3& normal)
-	: plane(normal, -dot(pt, normal)){};
+	: p(normal, -dot(pt, normal)){};
 	/// from 3 points
 	plane3(const vec3& p1, const vec3& p2, const vec3& p3)
 	: plane3(p1, normalize(cross(p2 - p1, p3 - p1))){};
-	LFORCEINLINE float operator[](size_t idx) const { return plane[idx]; };
-	LFORCEINLINE float& operator[](size_t idx) { return plane[idx]; };
-	LFORCEINLINE const float* toFloatPtr() const { return plane.toFloatPtr(); }
-	LFORCEINLINE float* toFloatPtr() { return plane.toFloatPtr(); }
-	LFORCEINLINE vec3 toVector3() const { return plane.toVector3(); }
+	LFORCEINLINE float operator[](size_t idx) const { return p[idx]; };
+	LFORCEINLINE float& operator[](size_t idx) { return p[idx]; };
+	LFORCEINLINE const float* toFloatPtr() const { return p.toFloatPtr(); }
+	LFORCEINLINE float* toFloatPtr() { return p.toFloatPtr(); }
+	LFORCEINLINE vec3 toVector3() const { return n; }
+	LFORCEINLINE vec3 getNormal() const { return n; }
+
+	LFORCEINLINE void invert() { n = -n; }
+	LFORCEINLINE plane3 getInverted() const { return plane3(-n, d); }
+
+	LFORCEINLINE float getDistanceToPointSigned(const vec3& pt) const { return n.dot(pt) + d; }
+	LFORCEINLINE float getDistanceToPoint(const vec3& pt) const { return ldr::absf(getDistanceToPointSigned(pt)); }
+
+	/// intersection of N-ray with this plane
+	LFORCEINLINE vec3 getPointOnPlane() const { return d * n / n.dot(n); }
+	/// project vector's ends to plane's normal
+	LFORCEINLINE vec3 projectToNormal(const vec3& v) const { return (v * n) * n; }
+	/// project vector's ends to plane
+	LFORCEINLINE vec3 projectPoint(const vec3& pt) const { return pt - projectToNormal(pt) + d * n; }
+
+	LFORCEINLINE void buildBasis(vec3* v1, vec3* v2) const
+	{
+		*v1 = n.getOrthogonalVector();
+		*v2 = normalize(cross(n, *v1));
+	}
 };
+
+static_assert(sizeof(plane3) == sizeof(vec4));
 
 } // namespace ldr
 
